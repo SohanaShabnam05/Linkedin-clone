80cd9e0bf7d4289b50b3042db90f2ec5c6967987
sohana <sshabnamzsf@gmail.com>
2023-01-02 02:26:57 +0530

    login2

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,6 +16,13 @@ const login = (props) => {
                     </SignIn>
                 </div>
             </Nav>
+        <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img src = "/images/login-hero.svg" alt=" " />
+                </Hero>
+        </Section>
+
         </Container>
     )
 };
@@ -64,6 +71,67 @@ const SignIn = styled.a`
     font-size : 16px;
     font-weight : 600;
     line-height : 40px;
+    padding : 10px 24px;
+    text-align : center;
+    background-color : rgba(0 , 0, 0,0 );
+    &:hover {
+        background-color : rgba(112, 181, 249, 0.15);
+        color : #0a66c2;
+        text-decoration : none;
+    }
+`;
+
+const Section = styled.a`
+    align-content : start;
+    display : flex;
+    min-height : 700px;
+    padding-bottom : 138px;
+    padding-top : 40px;
+    padding : 60px 0;
+    flex-wrap : wrap; 
+    ${'' /* //this is for responsive page wrapping. */}
+    width : 100%;
+    max-width : 1128px;
+
+    @media (max-width : 768px) {
+        margin : auto;
+        min-height : 0px;
+    }
+`;
+
+const Hero = styled.a`
+    width : 100%;
+    h1 {
+        padding-bottom: 0;
+        width : 55%;
+        font-size : 56px;
+        color : #2977c9;
+        font-weight : 200;
+        line-height : 70px;
+        @media (max-width : 768px)
+        {
+            text-align : center;
+            font-size : 20px;
+            width : 100%;
+            line-height : 2;
+        }
+    }
+
+    img {
+        z-index : -1;
+        width : 700px;
+        hight : 760px;
+        position : absolute;
+        boottom : -2px;
+        right : -150px;
+        @media (max-width : 760px)
+        {
+            top: 230px;
+            width : initial;
+            position : initial;
+            height : initial;
+        }
+    }
 `;
 
 export default login;
