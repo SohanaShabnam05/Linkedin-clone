87d62a3f1fa23c3e3e014337ee25f69b85123f58
sohana <sshabnamzsf@gmail.com>
2023-01-07 02:16:39 +0530

    main1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,63 @@
 import styled from "styled-components";
 
 const Main = (props) => {
-    return <Container>Main</Container>;
+    return (
+        <Container>
+            <ShareBox>Share</ShareBox>
+            <div>
+                <img src="/images/user.svg" alt="" />
+                <button>Start a post</button>
+            </div>
+
+            <div>
+                <button>
+                    <img src="/images/photo-icon.svg" alt="" />
+                    <span>Photo</span>
+                </button>
+            </div>
+            <div>
+                <button>
+                    <img src="/images/video-icon.svg" alt="" />
+                    <span>Video</span>
+                </button>
+
+                <button>
+                    <img src="/images/event-icon.svg" alt="" />
+                    <span>Event</span>
+                </button>
+                <button>
+                    <img src="/images/article-icon.svg" alt="" />
+                    <span>Write Article</span>
+                </button>
+            </div>
+
+        </Container>
+    );
 };
 
 const Container = styled.div`
-    grid-area : Main;
+    grid-area : main;
     
 `;
+
+const CommonCard = styled.div`
+    text-align : center;
+    overflow : hidden;
+    margin-bottom : 8px;
+    background-color : #fff;
+    border-radius : 5px;
+    position : relative;
+    border : none;
+    box-shadow : 0 0 0 1px rgba(0 0 0 /15%) , 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+const ShareBox = styled(CommonCard)`
+    display : flex;
+    flex-direction : column;
+    color : #958b7b;
+    margin : 0 0 8px;
+    background : white;
+`;
+
 
 export default Main;
