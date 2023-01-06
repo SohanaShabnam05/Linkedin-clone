adab1e3093f1a757c6093f0330d587935fe9f904
sohana <sshabnamzsf@gmail.com>
2023-01-06 19:10:34 +0530

    LeftSide

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,28 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = (props) => {
-    return
-    (
+    return (
     <Container>
         <Section>
             <h5>
                 <a>
                     Hiring in a hurry? - 
                 </a>
-                <p>Find talented pros in record time with Upwork and keep business</p>
-            </h5>
+
+                </h5>
+                <p>Find talented pros in record time with Upwork and keep business moving</p>
+           
         </Section>
+        <Layout>
+            <Leftside />
+            <Main />
+            <Rightside />
+        </Layout>
     </Container>
-    )
+    );
 };
 
 const Container = styled.div`
@@ -29,6 +38,48 @@ const Content = styled.div`
 
 const Section = styled.div`
     min-height : 50px;
+    padding : 16px 0;
+    box-sizing : content-box;
+    text-align : center;
+    text-decoration : underline;
+    display : flex;
+    justify-content : center;
+    h5 {
+        color : #0a66c2;
+        font-size : 14px;
+
+        a {
+            font-weight : 700;
+        }
+    }
+
+    p {
+        font-size : 14px;
+        color : #434649;
+        font-weight : 600;
+    }
+
+    @media (max-width : 768px)
+    {
+        flex-direction : column;
+        padding : 0 5px;
+    }
+`;
+
+const Layout = styled.div`
+    display : grid;
+    grid-template-areas : "leftside main rightside";
+    grid-template-columns : minmax(0, 5fx) minmax(0, 12fx) minmax(300px, 7fx);
+    column-gap : 25px;
+    row-gap : 25px;
+    margin : 25px 0;
+
+    @media( max-width: 768px)
+    {
+        display : flex;
+        flex-direction : column;
+        padding : 0 5px;
+    }
 `;
 
 export default Home;
