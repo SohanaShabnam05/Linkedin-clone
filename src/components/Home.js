a8e63051448949ed56eb799bc7136f610a81aeb3
sohana <sshabnamzsf@gmail.com>
2023-01-06 14:25:11 +0530

    Headerdone

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,32 @@ import styled from "styled-components";
 const Home = (props) => {
     return
     (
-    <div>
-        Home
-    </div>
+    <Container>
+        <Section>
+            <h5>
+                <a>
+                    Hiring in a hurry? - 
+                </a>
+                <p>Find talented pros in record time with Upwork and keep business</p>
+            </h5>
+        </Section>
+    </Container>
     )
 };
+
+const Container = styled.div`
+    padding-top : 52px;
+    max-width : 100%;
+`;
+
+const Content = styled.div`
+    max-width : 1128px;
+    margin-left : auto;
+    margin-right : auto;
+`;
+
+const Section = styled.div`
+    min-height : 50px;
+`;
 
 export default Home;
